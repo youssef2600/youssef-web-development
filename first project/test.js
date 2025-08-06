2d1a8327b26a622ee58c8024f6604ff0ec46ae58
youssef2600 <youssefchaouachi2017@gmail.com>
2025-08-06 12:39:50 +0100

    final

---
+++ first project/test.js
@@ -0,0 +1,10 @@
+const btn = document.querySelectorAll("button")
+const body = document.querySelector("body")
+btn[0].addEventListener("click", function (){
+    const number = parseInt(Math.random() * 1000000);
+    body.style.backgroundColor = "#" + number;
+});
+btn[1].addEventListener("click", function (){
+   document.getElementById("a").classList.toggle('none')
+
+});
