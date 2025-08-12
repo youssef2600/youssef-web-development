ffee6a0d8c006e976e0f22c75e6da73131cad6b2
youssef2600 <youssefchaouachi2017@gmail.com>
2025-08-12 11:20:58 +0100

    jkl kv h

---
+++ first project/test1.js
@@ -0,0 +1,42 @@
+console.log("hello world!");
+const x = 5;
+let y = 1500;
+console.log(x);
+
+//x = 15;
+/*x = "hello";
+console.log(x);*/
+
+//primitive data
+//strings
+let str = "hi! it's summer time";
+//numbers
+let z = 15.5;
+//boolean
+let bool = true;
+//no data
+let w;
+console.log(w);
+let nomoretears = null;
+//non primitive data
+//arrays
+let classRoom = [
+  "Oussayma",
+  "Arij",
+  "Aziz",
+  "Youssef",
+  "Amine",
+  "Ahmed",
+  "Elyes",
+];
+console.log(classRoom[3]);
+const ysf = {
+  firstname: "youssef",
+  lastname: "chaouachi",
+  age: 17,
+  present: true,
+};
+console.log(ysf);
+for (let i = 0; i < classRoom.length; i += 2) {
+  console.log(classRoom[i], " is present");
+}
